94550b7ed6c29222bdd9e6b0059d0cdfccdfd182
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-09-07 14:47:11 -0500

    database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,46 @@
 import { openDB } from 'idb';
+import { javascript } from 'webpack';
 
 const initdb = async () =>
+//Creating new database named "jate" which will be using version 1 or the database
   openDB('jate', 1, {
+    //Add our database schema if it has not already been initialized
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      //Create a new object store for the data and give it a key name of 'id' which needs to increment automatically.
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  //Create a connection to the database and the version we want to use
+  const jateDb = await openDB('jate', 1);
+  //Create a new transaction and specify the database abd data privileges
+  const tx = jateDb.transaction('jate', 'readWrite');
+  //Open up the desired object store
+  const store = tx.objectStore('jate');
+  //Use the add() method on the store and pass in the content
+  const request = store.add({ content });
+  //Get confirmation of the request
+  const result = await request;
+    console.log('Data saved to database', result);
+//What do we need the error for, come back to this
+   // console.error('putDb not implemented');
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  constjateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readOnly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  return result;
+};
+//console.error('getDb not implemented');
 
 initdb();
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,7 +1,7 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// TODO: Add an event handler to the `beforeinstallprompt` event-(DONE)
 window.addEventListener('beforeinstallprompt', (event) => {
     //Store the triggered events
     window.deferredPrompt = event;
@@ -9,7 +9,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// TODO: Implement a click event handler on the `butInstall` element-(DONE)
 butInstall.addEventListener('click', async () => {
 const promptEvent= window.defferedPrompt;
 if(!promptEvent){
@@ -23,7 +23,7 @@ window.deferredPrompt = null;
 butInstall.classList.toggle('hidden', true);
  });
 
-// TODO: Add an handler for the `appinstalled` event
+// TODO: Add an handler for the `appinstalled` event-(DONE)
 window.addEventListener('appinstalled', (event) => {
 //Clear prompt
 window.deferredPrompt = null;
